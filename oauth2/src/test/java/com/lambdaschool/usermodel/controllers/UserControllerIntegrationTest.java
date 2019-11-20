526ef375254ffe400fe6127a850e73d0589f5e98
Patrick Chow <PatrickChow0803@gmail.com>
2019-11-20 15:55:47 -0500

    Fixed Up Unit Testing

--- oauth2/src/test/java/com/lambdaschool/usermodel/controllers/UserControllerIntegrationTest.java
+++ oauth2/src/test/java/com/lambdaschool/usermodel/controllers/UserControllerIntegrationTest.java
@@ -24,7 +24,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+/////////////////////////////////////////////////////////////////////////////////////////////////
 /**
  * Integration test for UserController so only looking at 100% coverage on UserController
  */
